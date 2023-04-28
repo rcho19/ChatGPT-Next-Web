8253625b28d951b213364093d6e0405ff9c6756b
侯睿成 <441202692@qq.com>
2023-04-28 10:41:54 +0800

    fix: custom modification

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,9 +96,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>My ChatGPT</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          我的AI助手
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
